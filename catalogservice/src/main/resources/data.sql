e36cd19a1a57eba261ee5689f6277d23a149ea23
unknown <201303470@o.cnu.ac.kr>
2021-12-10 12:06:35 +0900

    catalog microservice setting

---
+++ catalogservice/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-001', 'Ber', 100, 1500);
+
+
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-002', 'tk', 111, 1400);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-003', 'kor', 200, 5500);
